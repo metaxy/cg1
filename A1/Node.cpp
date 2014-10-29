fe19e1c06a55011fc9e2aaf9088a1a1b32c43934
mworchel <mworchel@hotmail.de>
2014-10-29 17:11:12 +0100

    code wiederhergestellt

--- A1/Node.cpp
+++ A1/Node.cpp
@@ -1,12 +1,12 @@
 /* ----------------------------------------------------------------
-   name:           Node.cpp
-   purpose:        implementation for a scenegraph node
-   version:	   SKELETON CODE
-   TODO:           transform, drawJoint
-   author:         katrin lang
-		   computer graphics
-		   tu berlin
-   ------------------------------------------------------------- */
+name:           Node.cpp
+purpose:        implementation for a scenegraph node
+version:	   SKELETON CODE
+TODO:           transform, drawJoint
+author:         katrin lang
+computer graphics
+tu berlin
+------------------------------------------------------------- */
 
 #include <iostream>
 #ifdef __APPLE__ 
@@ -24,91 +24,122 @@
 // printline debugging won't work
 using namespace std;
 
-  // constructor
-  // x,y,z: position of node center of mass relative to parent node center
-  // length, height, and width of this body part
-  // jointx, jointy, jointz: x,y,z offset of  rotation axis from center of body part
-  // i.e. for rotation about a "shoulder" as opposed to the center of mass of a glutsolidcube
-  // x,y,z: rotation (in degrees) of node relative to parent node rotation
-  Node::Node(float x, float y, float z, 
-       float length, float height, float width,
-       float jointx, float jointy, float jointz,
-       float rotx, float roty, float rotz){
+// constructor
+// x,y,z: position of node center of mass relative to parent node center
+// length, height, and width of this body part
+// jointx, jointy, jointz: x,y,z offset of  rotation axis from center of body part
+// i.e. for rotation about a "shoulder" as opposed to the center of mass of a glutsolidcube
+// x,y,z: rotation (in degrees) of node relative to parent node rotation
+Node::Node(float x, float y, float z,
+		   float length, float height, float width,
+		   float jointx, float jointy, float jointz,
+		   float rotx, float roty, float rotz) {
 
-    this->selected= false;
+	this->selected = false;
 
-    this->x= x;
-    this->y= y;
-    this->z= z;
-    
-    this->length= length;
-    this->height= height;
-    this->width= width;
-    
-    this->jointx= jointx;
-    this->jointy= jointy;
-    this->jointz= jointz;
+	this->x = x;
+	this->y = y;
+	this->z = z;
 
-    this->rotx= rotx;
-    this->roty= roty;
-    this->rotz= rotz;
+	this->length = length;
+	this->height = height;
+	this->width = width;
 
-    this->parent= NULL;
-    this->child= NULL;
-    this->previous= NULL;
-    this->next= NULL;
-  }
+	this->jointx = jointx;
+	this->jointy = jointy;
+	this->jointz = jointz;
+
+	this->rotx = rotx;
+	this->roty = roty;
+	this->rotz = rotz;
+
+	this->parent = NULL;
+	this->child = NULL;
+	this->previous = NULL;
+	this->next = NULL;
+}
 
 // destructor
 // nothing to do so far
-Node::~Node(){}
+Node::~Node() {
+}
 
 // sets the parent of this node
 // and adds this node 
 // to the list of the parent's children
-void Node::setParent(Node* parent){
-  
-  this->parent= parent;
-  if(parent->child==NULL){
-    parent->child= this;
-  }
-  else{
-    Node *sibling= parent->child;
-    while(sibling->next != NULL) sibling= sibling->next;
-    sibling->next= this;
-    this->previous= sibling;
-  }
+void Node::setParent(Node* parent) {
+
+	this->parent = parent;
+	if(parent->child == NULL) {
+		parent->child = this;
+	} else {
+		Node *sibling = parent->child;
+		while(sibling->next != NULL) sibling = sibling->next;
+		sibling->next = this;
+		this->previous = sibling;
+	}
 }
 
-  // transform an individual node
-  // according to its position, 
-  // rotation, and rotation center
-  // and call draw() methods
-  // XXX: NEEDS TO BE IMPLEMENTED
-void Node::render(){
- 
+// transform an individual node
+// according to its position, 
+// rotation, and rotation center
+// and call draw() methods
+// XXX: NEEDS TO BE IMPLEMENTED
+void Node::render() {
+
 	// note the order of the operations:
 	// the transformations are applied in "reverse" order
 	// of glRotate/glTranslate calls
 	// (also see cg1 lecture notes on this topic)
 
 	// translate to final position
+	// XXX
+
+	// INSERT YOUR CODE HERE
+	glTranslatef(x, y, z);
+
+	// END XXX
+
 	// translate node center to joint position
+	// XXX
+
+	// INSERT YOUR CODE HERE
+	glTranslatef(jointx, jointy, jointz);
+
+	// END XXX
+
 	// apply this node's rotation
+	// XXX
+
+	// INSERT YOUR CODE HERE
+	glRotatef(rotx, 1.f, 0.f, 0.f);
+	glRotatef(roty, 0.f, 1.f, 0.f);
+	glRotatef(rotz, 0.f, 0.f, 1.f);
+
+	// END XXX
+
 	// draw Joint (rotation center)
 	drawJoint();
+
 	// translate center of rotation into limb's origin
+	// XXX
+
+	// INSERT YOUR CODE HERE
+	glTranslatef(-jointx, -jointy, -jointz);
+
+	// END XXX
+
 	// draw the node
 	draw();
 }
 
 // draw an individual node
-void Node::draw(){
+void Node::draw() {
 
 	// save lighting bit for red light
 	glPushAttrib(GL_LIGHTING_BIT);
 
-	GLfloat red[]= {1.0, 0, 0};
+	GLfloat red[] = {1.0, 0, 0};
 	if(selected) glLightModelfv(GL_LIGHT_MODEL_AMBIENT, red);
 
 	glPushMatrix();
@@ -124,7 +155,7 @@ void Node::draw(){
 // draw the joint (rotation center)
 // of an individual node
 // XXX: NEEDS TO BE IMPLEMENTED
-void Node::drawJoint(){
+void Node::drawJoint() {
 
 	// save enable bit for lighting
 	// and current bit for color
@@ -139,6 +170,8 @@ void Node::drawJoint(){
 	// XXX
 
 	// INSERT YOUR CODE HERE
+	glColor3f(1.f, 0.7f, 0.7f);
+	glutWireSphere(30.f, 10.f, 10.f);
 
 	// END XXX
 
@@ -148,6 +181,18 @@ void Node::drawJoint(){
 	//      SEE PROVIDED cg1_ex1.exe (win32) AND cg1_ex1 (linux)
 
 	// INSERT YOUR CODE HERE
+	glColor3f(1.f, 0.f, 0.f);
+	glVertex3f(50.f, 0.f, 0.f);
+	glVertex3f(0.f, 0.f, 0.f);
+
+	glColor3f(0.f, 1.f, 0.f);
+	glVertex3f(0.f, 50.f, 0.f);
+	glVertex3f(0.f, 0.f, 0.f);
+
+	glColor3f(0.f, 0.f, 1.f);
+	glVertex3f(0.f, 0.f, 50.f);
+	glVertex3f(0.f, 0.f, 0.f);
+
 
 	// END XXX
 
@@ -156,34 +201,34 @@ void Node::drawJoint(){
 }
 
 // increment / decrement rotation
-void Node::rotate(float x, float y, float z){
-  
-  rotx+= x;
-  roty+= y;
-  rotz+= z;
+void Node::rotate(float x, float y, float z) {
+
+	rotx += x;
+	roty += y;
+	rotz += z;
 }
 
-Node *Node::getNext(){
-  return next;
+Node *Node::getNext() {
+	return next;
 }
 
-Node *Node::getPrevious(){
-  return previous;
+Node *Node::getPrevious() {
+	return previous;
 }
 
-Node *Node::getParent(){
-  return parent;
+Node *Node::getParent() {
+	return parent;
 }
 
-Node *Node::getChild(){
-  return child;
+Node *Node::getChild() {
+	return child;
 }
 
-void Node::select(){
-  selected= true;
+void Node::select() {
+	selected = true;
 }
 
-void Node::deselect(){
-  selected= false;
+void Node::deselect() {
+	selected = false;
 }
 
--- A1/Scenegraph.cpp
+++ A1/Scenegraph.cpp
@@ -1,12 +1,12 @@
 /* ----------------------------------------------------------------
-   name:           Scenegraph.cpp
-   purpose:        implementation of the scenegraph functions
-   version:	   SKELETON CODE
-   TODO:           traverse reset
-   author:         katrin lang
-		   computer graphics
-		   tu berlin
-   ------------------------------------------------------------- */
+name:           Scenegraph.cpp
+purpose:        implementation of the scenegraph functions
+version:	   SKELETON CODE
+TODO:           traverse reset
+author:         katrin lang
+computer graphics
+tu berlin
+------------------------------------------------------------- */
 
 #include <iostream>
 #ifdef __APPLE__ 
@@ -22,123 +22,135 @@
 // without this line, 
 // printline debugging won't work
 using namespace std;
- 
+
 // this is our scenegraph
 SceneGraph *sceneGraph;
 
 SceneGraph::SceneGraph(Node *root) : root(root), selected(root) {
-  selected->select();
+	selected->select();
 }
 
-SceneGraph::~SceneGraph(){
-  clear(root);
+SceneGraph::~SceneGraph() {
+	clear(root);
 }
 
 // traverse and draw the scenegraph
 // nothing to do here
 // (see helper function)
-void SceneGraph::traverse(){
-  traverse(root);
+void SceneGraph::traverse() {
+	traverse(root);
 }
 
 // reset all rotations in the scenegraph
 // nothing to do here
 // (see helper function)
-void SceneGraph::reset(){
-  reset(root);
+void SceneGraph::reset() {
+	reset(root);
 }
 
 // navigation in tree
 // (needed for node selection)
-void SceneGraph::up(){
-  if(selected->getParent() == NULL) return;
-    selected->deselect();
-    selected= selected->getParent();
-    selected->select();
+void SceneGraph::up() {
+	if(selected->getParent() == NULL) return;
+	selected->deselect();
+	selected = selected->getParent();
+	selected->select();
 }
-void SceneGraph::down(){
-  if(selected->getChild() == NULL) return;
-    selected->deselect();
-    selected= selected->getChild();
-    selected->select();
+void SceneGraph::down() {
+	if(selected->getChild() == NULL) return;
+	selected->deselect();
+	selected = selected->getChild();
+	selected->select();
 }
-void SceneGraph::left(){
-  if(selected->getPrevious() == NULL) return;
-  selected->deselect();
-  selected= selected->getPrevious();
-  selected->select();
+void SceneGraph::left() {
+	if(selected->getPrevious() == NULL) return;
+	selected->deselect();
+	selected = selected->getPrevious();
+	selected->select();
 }
-void SceneGraph::right(){ 
-  if(selected->getNext() == NULL) return;
-  selected->deselect();
-  selected= selected->getNext();
-  selected->select();
+void SceneGraph::right() {
+	if(selected->getNext() == NULL) return;
+	selected->deselect();
+	selected = selected->getNext();
+	selected->select();
 }
 
 // increment / decrement rotation of selected node
-void SceneGraph::rotate(float x, float y, float z){
-  selected->rotate(x, y, z);
+void SceneGraph::rotate(float x, float y, float z) {
+	selected->rotate(x, y, z);
 }
 
 // traverse and draw the scenegraph from a given node
 // XXX: NEEDS TO BE IMPLEMENTED
-void SceneGraph::traverse(Node *node){
+void SceneGraph::traverse(Node *node) {
 
-  if(node == NULL) return;
+	if(node == NULL) return;
 
-  // store current modelview (transformation) matrix
-  // XXX
-  // INSERT YOUR CODE HERE
-  
-  // END XXX
-  
-  // apply local transformation
-  // and draw current node
-  node->render();
-	
-  // continue concatenating transformations
-  // if this node has children
-  // XXX  
+	// store current modelview (transformation) matrix
+	// XXX
 
-  // INSERT YOUR CODE HERE
+	// INSERT YOUR CODE HERE
+	glPushMatrix();
 
-  // END XXX
-  
-  // after this scenegraph branch
-  // restore previous transformation
-  // XXX
-  
-  // INSERT YOUR CODE HERE
-  
-  // END XXX
-  
-  // and traverse possible siblings
-  // XXX
-  
-  // INSERT YOUR CODE HERE
-  
-  // END XXX
+	// END XXX
+
+	// apply local transformation
+	// and draw current node
+	node->render();
+
+	// continue concatenating transformations
+	// if this node has children
+	// XXX  
+
+	// INSERT YOUR CODE HERE
+	traverse(node->getChild());
+
+	// END XXX
+
+	// after this scenegraph branch
+	// restore previous transformation
+	// XXX
+
+	// INSERT YOUR CODE HERE
+	glPopMatrix();
+
+	// END XXX
+
+	// and traverse possible siblings
+	// XXX
+
+	// INSERT YOUR CODE HERE
+	traverse(node->getNext());
+
+	// END XXX
 }
 
-void SceneGraph::clear(Node *node){
+void SceneGraph::clear(Node *node) {
 
-  if(node == NULL) return;
+	if(node == NULL) return;
 
-  clear(node->getChild());
-  clear(node->getNext());
+	clear(node->getChild());
+	clear(node->getNext());
 
-  delete node;
+	delete node;
 
 }
 
 // reset all rotations below a given node
 // XXX: NEEDS TO BE IMPLEMENTED
-void SceneGraph::reset(Node* node){
+void SceneGraph::reset(Node* node) {
+	if(node == nullptr) return;
 
-  // reset rotations
-  // XXX
-  
-  // INSERT YOUR CODE HERE
-  
-  // END XXX
+	// reset rotations
+	// XXX
+
+	// INSERT YOUR CODE HERE
+	node->rotx = 0.f;
+	node->roty = 0.f;
+	node->rotz = 0.f;
+
+	reset(node->getChild());
+
+	reset(node->getNext());
+	// END XXX
 }
